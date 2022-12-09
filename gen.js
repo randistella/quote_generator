824acb1c6b2bf3d007159356b93f577435c29f7e
Randi Stella <randinicolestella@gmail.com>
2022-12-09 09:48:22 -0600

    added dog pic function

--- gen.js
+++ gen.js
@@ -1,6 +1,5 @@
 let quotes = [
     'Everything I know I learned from dogs. – Nora Roberts',
-    'Dogs do speak, but only to those who know how to listen.– Orhan Pamuk',
     'Dogs lives are too short. Their only fault, really. – Agnes Sligh Turnbull',
     'Dogs are not our whole life, but they make our lives whole. – Roger Caras',
     'The better I get to know men, the more I find myself loving dogs. – Charles De Gaulle',
@@ -8,7 +7,21 @@ let quotes = [
     'The dog is a gentleman; I hope to go to his heaven not man’s. – Mark Twain',
 ]
 
+let images = new Array (
+    'dog_1.jpeg',
+   'dog_2.jpeg',
+    'dog_3.jpeg',
+    'dog_4.jpg',
+    'dog_5.jpg',
+    'dog_6.jpeg',
+);
+
 function newQuote() {
     let randomNumber = Math.floor(Math.random() * quotes.length);
     document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
+
+function newDog() {
+    let randomNumber = Math.floor(Math.random() * images.length);
+    document.getElementById('dogDisplay').src = images[randomNumber];
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+Final Project
+Author: Randi Stella
+Date:   Nov 11th, 2022
+Purpose: Use of arrays and random generator to display a quote and dog picture when user clicks on button.
